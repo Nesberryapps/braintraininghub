a897e71173b6488c044e682f6701b2dc2578be9f
Nesberry LLC <nesberry.apps@gmail.com>
2025-11-26 18:02:45 +0000

    Before we move forward, I just noticed this security issue.  What does t

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "mind-bloom-v3y6p",
-  "appId": "1:1080250387073:web:5c8f448091545408fedb80",
-  "storageBucket": "mind-bloom-v3y6p.firebasestorage.app",
-  "apiKey": "AIzaSyDRYYcd8jNO1PLscSSMlnkp-QiO-A9OGO0",
-  "authDomain": "mind-bloom-v3y6p.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1080250387073"
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
 };
 
 // Initialize Firebase
