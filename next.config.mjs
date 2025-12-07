042407554d7e139987fc96b83b3371e091c8c6fa
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-07 06:52:55 +0000

    Fix next.config file extension

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type {NextConfig} from 'next';
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   output: 'export',
   typescript: {
@@ -23,3 +22,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
