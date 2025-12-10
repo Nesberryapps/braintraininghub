abf92dbc4d8cb3979256d2eae6c6f26225022102
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-10 01:57:03 +0000

    So now that the app has been approved and is officially published on the

--- src/services/admob.tsx
+++ src/services/admob.tsx
@@ -13,7 +13,7 @@ export async function initializeAdMob() {
 
 const AppStoreIcons = () => (
     <div className="flex flex-col gap-4">
-        <p>To get hints and other bonuses, please download our mobile app. The app is currently in review and will be available on the App Store and Google Play Store soon!</p>
+        <p>To get hints and other bonuses, please download our free mobile app from the App Store or Google Play Store!</p>
         <div className="flex gap-4 items-center">
             {/* Apple App Store Icon */}
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z"></path><path d="M10 2c1 .5 2 2 2 5"></path></svg>
