303b5bf26f283ab019b0c298215ee99baa5b038d
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-15 20:09:55 +0000

    What are your thoughts of implementing an notification feature that noti

--- android/app/src/main/java/com/nesberry/braintraining/MainActivity.java
+++ android/app/src/main/java/com/nesberry/braintraining/MainActivity.java
@@ -1,5 +1,20 @@
 package com.nesberry.braintraining;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.community.admob.AdMob;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      add(AdMob.class);
+    }});
+  }
+}
