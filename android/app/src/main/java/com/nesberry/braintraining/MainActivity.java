4ed9bcf36ff32cada26b233380811dd4b53c3f28
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-15 21:07:22 +0000

    As oer the screenshot the android build in github failed

--- android/app/src/main/java/com/nesberry/braintraining/MainActivity.java
+++ android/app/src/main/java/com/nesberry/braintraining/MainActivity.java
@@ -1,20 +1,5 @@
 package com.nesberry.braintraining;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
 
-import com.getcapacitor.community.admob.AdMob;
-import com.getcapacitor.Plugin;
-
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      add(AdMob.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
