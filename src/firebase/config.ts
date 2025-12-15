e34dff98a1710a4a4705366baa1147d56ce4ea3b
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-15 20:17:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "mind-bloom-v3y6p",
+  "appId": "1:1080250387073:web:5c8f448091545408fedb80",
+  "apiKey": "AIzaSyDRYYcd8jNO1PLscSSMlnkp-QiO-A9OGO0",
+  "authDomain": "mind-bloom-v3y6p.firebaseapp.com",
+  "measurementId": "G-5V93ET4Z5H",
+  "messagingSenderId": "1080250387073"
+};
