8e00b6ddeb0773b5cf59ef76d2a39b24157429f3
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-19 04:49:24 +0000

    Fix iOS script syntax and Android package name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nesberry.braintraining',
+  appId: 'com.nesberry.braintraininghub',
   appName: 'Brain Training Hub',
   webDir: 'out',
   server: {
