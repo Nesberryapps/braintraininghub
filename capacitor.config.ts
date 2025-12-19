07aa7754b80f42849fff54c2fe648848d491970c
Nesberry LLC <nesberry.apps@gmail.com>
2025-12-19 06:21:00 +0000

    Revert ID to com.nesberry.braintraining to match Store

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.nesberry.braintraininghub',
+  appId: 'com.nesberry.braintraining',
   appName: 'Brain Training Hub',
   webDir: 'out',
   server: {
