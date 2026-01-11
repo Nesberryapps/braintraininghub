34df3b86abe57e2ca58fd23f6b5154b277f89342
Nesberry LLC <nesberry.apps@gmail.com>
2026-01-11 04:31:13 +0000

    I want to change my rsCAPTCHA key as it was exposed in github.  how do i

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6439fba7cbdafacf"; }
+/******/ 		__webpack_require__.h = function() { return "2fd099dfcf6260e5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
